486c4315dcdba79d9e78466aaeaf3ea19c01bdc3
alcidesjunior <alcidesjunior.infor@gmail.com>
2017-09-03 16:36:28 -0300

    questao bugada

--- lista2/modules/questao56.h
+++ lista2/modules/questao56.h
@@ -1,79 +1,85 @@
 void questao56(){
   // system("clear");
   // questao 5
-  struct appoiment_book{
+  typedef struct appoiment_book{
     char name[60];
     char address[200];
     char tel[15];
     char status[1];
-  };
+  } Appoiment_book;
 
   // questão 6
-  int action,i;
-  struct appoiment_book apmt[2];
-  char *searchtable;
+  int action,i,index=0;
+  char *string_search;
+  Appoiment_book apmt[2];
+
+  // menu
+  void drawn(int qtd, char symbol){
+    for(i=0;i<qtd;i++){
+      printf("%c",symbol);
+    }
+  }
+  // esqueleto do menu
   void menu(){
-    printf("============MENU=============\n");
-    printf("0 - para inserir registros\n");
-    printf("1 - para realizar pesquisa via nome\n");
-    printf("2 - para atualizar um registro\n");
-    printf("3 - para voltar\n");
-    printf("4 - para sair do programa\n");
-    printf("=============================\n");
-    scanf("%d",&action);
-    system("clear");
-    fflush(stdin); //usado no para limpar buffer do teclado no Windows
-    __fpurge(stdin);//usado para limpar buffer do teclado no Linux <
+    drawn(40,'*');
+    printf("\n\t\tMENU\n");
+    drawn(40,'*');
+    printf("\n");
+    printf("\tEscolha uma opção abaixo:\n");
+    printf("\t1 - Novo registro\n\t2 - Pesquisar registro\n\t3 - Alterar registro\n\t4 - Sair\n");
+    drawn(40,'*');
+    printf("\n");
   }
-  void insert(){
-    for (i = 0; i < 2; i++) {
-      printf("Nome: ");
-      fgets(apmt[i].name,sizeof(apmt[i].name),stdin);
-      fflush(stdin); //usado no para limpar buffer do teclado no Windows
-      __fpurge(stdin);//usado para limpar buffer do teclado no Linux <3
-      printf("Endereço: ");
-      fgets(apmt[i].address,sizeof(apmt[i].address),stdin);
-      fflush(stdin); //usado no para limpar buffer do teclado no Windows
-      __fpurge(stdin);//usado para limpar buffer do teclado no Linux <3
-      printf("Telefone: ");
-      fgets(apmt[i].tel,sizeof(apmt[i].tel),stdin);
-      fflush(stdin); //usado no para limpar buffer do teclado no Windows
-      __fpurge(stdin);//usado para limpar buffer do teclado no Linux <3
-      printf("Status: ");
-      fgets(apmt[i].name,sizeof(apmt[i].name),stdin);
-      fflush(stdin); //usado no para limpar buffer do teclado no Windows
-      __fpurge(stdin);//usado para limpar buffer do teclado no Linux <3
-    }
-  }
-  void search(){
-    printf("Insira o nome a ser pesquisado: ");
-    scanf("%s",searchtable);
-    // fgets(searchtable,sizeof(searchtable),stdin);
-    fflush(stdin);
+  void insert(struct appoiment_book* apm,int index){
+    printf("Nome: ");
+    fgets(apm[index].name,sizeof(apm[index].name),stdin);
     __fpurge(stdin);
-    for(i = 0;i < 2;i++){
-      if(strcmp(apmt[i].name,searchtable) == 0){
-        printf("%s\n",apmt[i].name);
-        printf("%s\n",apmt[i].address);
-        printf("%s\n",apmt[i].tel);
-        printf("%s\n",apmt[i].status);
-      }
-    }
+    getchar();
+    printf("Endereço: ");
+    fgets(apm[index].address,sizeof(apm[index].address),stdin);
+    __fpurge(stdin);
+    getchar();
+    printf("Telefone: ");
+    fgets(apm[index].tel,sizeof(apm[index].tel),stdin);
+    __fpurge(stdin);
+    getchar();
+    printf("Status: [L - Livre | O - Ocupado | * Apagado] ");
+    fgets(apm[index].status,sizeof(apm[index].status),stdin);
+    __fpurge(stdin);
+    getchar();
+    printf("\n%s",apm[index].name);
   }
-  void update(){
-    printf("atualizando");
+  void find(struct appoiment_book* apm,char *search,int index){
+    // for(i=0;i<=index;i++){
+      // if(strcmp(apmt[i].name, search)==0){
+        printf("%s",apm[0].name);//apm[i].name);
+      // }
+    // }
   }
+  // funcionalidade de fato
   do{
     menu();
-    switch (action) {
-      case 0:
-        insert(); break;
-      case 1:
-        search(); break;
-      case 2:
-        update(); break;
-      case 3:
-        menu(); break;
+    scanf("%d",&action);
+    system("clear");
+    if(action == 1){
+      if(index < 2){
+        insert(apmt, index);
+        index += 1;
+      }else{
+        drawn(40,'#');
+        printf("Para inserir mais registros, favor, comprar a licença PREMIUM pelo email:\n");
+        printf("chupaquedeuva@softwares.com\n");
+        drawn(40,'#');
+      }
+    }else if(action == 2){
+      printf("Nome a ser pesquisado:(pressione enter para sair)");
+      scanf("%s",string_search);
+      // fgets(string_search,sizeof(string_search),stdin);
+      // __fpurge(stdin);
+
+      find(apmt,string_search,index);
+    }else if(action == 3){
+
     }
   }while(action != 4);
 }
