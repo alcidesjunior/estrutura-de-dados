8727e591442c0cb8bc60dc2e8cafe3c60168289a
alcidesjunior <alcidesjunior.infor@gmail.com>
2017-09-03 18:05:39 -0300

    matriz

--- lista2/main.c
+++ lista2/main.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+#include "modules/questao1.h"
 #include "modules/questao2.h"
 #include "modules/questao34.h"
 #include "modules/questao56.h"
 
 void main(){
+  questao1();
   // questao2();
   // questao34();
-  questao56();
+  // questao56();
 }
